b98d3c4e1422ce641efe0a83fb0c43233077d7de
Daniel Ramirez <javamonn@gmail.com>
2015-04-05 23:50:12 -0400

    add gitignore and npm deps

---
+++ js/components/todo-app/todo-app.directive.js
@@ -0,0 +1,36 @@
+(function() {
+  'use strict';
+  
+  angular
+    .module('app')
+    .directive('todoApp', ['TodoStore', todoApp]);
+
+  function todoApp(TodoStore) {
+
+    /**
+     * Retrieve the current TODO data from the TodoStore.
+     */
+    function getTodoState() {
+      return {
+        allTodos: TodoStore.getAll(),
+        areAllComplete: TodoStore.areAllComplete()
+      };
+    }
+
+    function link(scope, elem, attrs) {
+      scope.todoState = getTodoState();
+
+      /**
+       * Event handler for 'change' events coming from the TodoStore.
+       */
+      TodoStore.addChangeListener(function() {
+        scope.todoState = getTodoState();
+      });
+    }
+
+    return {
+      restrict: 'A',
+      templateUrl: 'components/todo-app.html',
+      link: link
+    };
+  }
