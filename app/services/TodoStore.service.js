75d5fa7f7a92997b039af1ebc3f03d8cc37a0e8a
Daniel Ramirez <javamonn@gmail.com>
2015-05-31 13:24:19 -0400

    refactor to represent todos internally

--- app/services/TodoStore.service.js
+++ app/services/TodoStore.service.js
@@ -8,45 +8,51 @@
   });
 
   let TodoStore = function(TodoActions, PersistStore) {
-    return Reflux.createStore({
+
+    let _todos;
+
+    let todoStore = Reflux.createStore({
       listenables: [TodoActions],
       onCreate: function(text) {
-        this.updateTodos(this._todos.push(new TodoRecord({
+        _updateTodos(_todos.push(new TodoRecord({
           text: text,
-          id: cuid(),
+          id: cuid()
         })));
       },
       onUpdateText:function(id, text) {
-        let [index, todo] = this._todos.findEntry(todo => todo.id == id);
-        this.updateTodos(this._todos.set(index, todo.set("text", text)))
+        let [index, todo] = _todos.findEntry(todo => todo.id == id);
+        _updateTodos(_todos.set(index, todo.set("text", text)))
       },
       onToggleComplete: function(id) {
-        let [index, todo] = this._todos.findEntry(todo => todo.id == id);
-        this.updateTodos(this._todos.set(index, todo.set("complete", !todo.complete)));
+        let [index, todo] = _todos.findEntry(todo => todo.id == id);
+        _updateTodos(_todos.set(index, todo.set("complete", !todo.complete)));
       }, 
       onToggleCompleteAll: function(checked) {
-        this.updateTodos(this._todos.map(todo => todo.set("complete", checked)));
+        _updateTodos(_todos.map(todo => todo.set("complete", checked)));
       },
       onDestroy: function(id) {
-        let [index, todo] = this._todos.findEntry(todo => todo.id == id);
-        this.updateTodos(this._todos.delete(index));
+        let [index, todo] = _todos.findEntry(todo => todo.id == id);
+        _updateTodos(_todos.delete(index));
       },
       onDestroyCompleted: function() {
-        this.updateTodos(this._todos.filter(todo => !todo.complete));
-      },
-      updateTodos: function(todos) {
-        this._todos = todos;
-        this.trigger(this._todos);
-        PersistStore.update(todos);
+        _updateTodos(_todos.filter(todo => !todo.complete));
       },
       initialize: function() {
         return PersistStore.initialize()
           .then(todos => {
-             this._todos = todos;
+             _todos = todos;
              return todos;
           });
       }
     });
+
+    let _updateTodos = function(todos) {
+      _todos = todos;
+      todoStore.trigger(_todos);
+      PersistStore.update(todos);
+    };
+
+    return todoStore;
   };
 
   angular
